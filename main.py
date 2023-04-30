cf84902e08f2f622e0e9978178e8db6d9eebbdb4
Julius Park <juliusgeo@gmail.com>
2023-04-30 03:35:00 -0700

    update benchmarking script

--- main.py
+++ main.py
@@ -36,18 +36,16 @@ num_trials = 20
 for size in sizes:
     test_arr = list(range(size))
     value=size//30
-    #times_me.append(timeit(lambda:branchless_bisect_left(test_arr, value), number=num_trials))
-    #test_arr2 = SortedList(test_arr)
-    #times.append(timeit(lambda:bisect_left(test_arr2, value), number=num_trials))
+    times_me.append(timeit(lambda:branchless_bisect_left(test_arr, value), number=num_trials))
+    test_arr2 = SortedList(test_arr)
+    times.append(timeit(lambda:bisect_left(test_arr2, value), number=num_trials))
     times_c.append(timeit(lambda:bisect_left_c(test_arr, value), number=num_trials))
     times_bl_c.append(timeit(lambda:bl_bisect_left(test_arr, value), number=num_trials))
-    #print(bisect_left_c(test_arr,value))
-    #print(bl_bisect_left(test_arr, value))
-    #assert bisect_left_c(test_arr, value) == bl_bisect_left(test_arr, value)
+    assert len(set([i(test_arr2 if i==bisect_left else test_arr, value) for i in [bisect_left, branchless_bisect_left, bisect_left_c, bl_bisect_left]])) ==1
 
-#plt.plot(sizes, times, label="bisect_left")
+plt.plot(sizes, times, label="bisect_left")
 plt.plot(sizes, times_c, label="bisect_left_c")
-#plt.plot(sizes, times_me, label="branchless_bisect_left")
+plt.plot(sizes, times_me, label="branchless_bisect_left")
 plt.plot(sizes, times_bl_c, label="branchless_bisect_left_c")
 plt.xlabel('Input Size')
 plt.ylabel('Execution Time (s)')
